2ae1a9b146542760f37060694b1960cfb071a506
Warren Yao <warrenyca905@gmail.com>
2019-06-29 04:03:54 +0000

    commit

---
+++ lib/cli.rb
@@ -0,0 +1,42 @@
+require_relative "../lib/pokemon.rb"
+require_relative "../lib/scraper.rb"
+require_relative "../lib/type.rb"
+require 'nokogiri'
+require 'colorize'
+
+class CommandLineInterface
+  BASE_PATH = "https://bulbapedia.bulbagarden.net/wiki/"
+
+  def run
+    make_pokemon
+    add_attributes_to_students
+    display_students
+  end
+
+  def make_pokemon
+    info_hash = Scraper.scrape_page(BASE_PATH + "Pikachu_(Pokémon)") #replace pikachu with interpolation
+    Student.create_from_collection(info_hash)
+  end
+
+  def add_attributes_to_students
+    Student.all.each do |student|
+      attributes = Scraper.scrape_profile_page(BASE_PATH + student.profile_url)
+      student.add_student_attributes(attributes)
+    end
+  end
+
+  def display_students
+    Student.all.each do |student|
+      puts "#{student.name.upcase}".colorize(:blue)
+      puts "  location:".colorize(:light_blue) + " #{student.location}"
+      puts "  profile quote:".colorize(:light_blue) + " #{student.profile_quote}"
+      puts "  bio:".colorize(:light_blue) + " #{student.bio}"
+      puts "  twitter:".colorize(:light_blue) + " #{student.twitter}"
+      puts "  linkedin:".colorize(:light_blue) + " #{student.linkedin}"
+      puts "  github:".colorize(:light_blue) + " #{student.github}"
+      puts "  blog:".colorize(:light_blue) + " #{student.blog}"
+      puts "----------------------".colorize(:green)
+    end
+  end
+
+end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,40 +5,35 @@ require 'pry'
 require_relative './course.rb'
 
 class Scraper
-  
-  def get_page
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    # doc.css(".post").each do |post|
-    #   course = Course.new
-    #   course.title = post.css("h2").text
-    #   course.schedule = post.css(".date").text
-    #   course.description = post.css("p").text
-    # end
+  @@pokemon_entry = []
+  @@pokemon_collect = []
+  :name, :type, :entry, :weight, :height, :generation, :stats, :evolution, :gender
+  def scrape_page(pokemon_url)
+    page = Nokogiri::HTML(open(pokemon_url))
+    
   end
   
-  def get_courses
-    self.get_page.css(".post")
-  end
+  # def generation
+  #   self.get_page.css(".post")
+  # end
   
-  def make_courses
-    self.get_courses.each do |post|
-      course = Course.new
-      course.title = post.css("h2").text
-      course.schedule = post.css(".date").text
-      course.description = post.css("p").text
-    end
-  end
+  # def make_courses
+  #   self.get_courses.each do |post|
+  #     course = Course.new
+  #     course.title = post.css("h2").text
+  #     course.schedule = post.css(".date").text
+  #     course.description = post.css("p").text
+  #   end
+  # end
   
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
+  # def print_courses
+  #   self.make_courses
+  #   Course.all.each do |course|
+  #     if course.title
+  #       puts "Title: #{course.title}"
+  #       puts "  Schedule: #{course.schedule}"
+  #       puts "  Description: #{course.description}"
+  #     end
+  #   end
+  # end
 end
-
-Scraper.new.get_page
