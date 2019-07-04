7182ee55a2666129dc333dd68238f4424ce214a4
Warren Yao <warrenyca905@gmail.com>
2019-07-04 18:28:40 +0000

    restructuring cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -17,44 +17,51 @@ class CommandLineInterface
     puts "I contain information on all of the 151 Kanto Region Pokémon."
     puts "What would you like to do?"
     
-    menu(input)
-    input = gets.strip.downcase
+    start
 
-    while input != "exit" do
-      if input == "list all"
-        puts "How would you like to list?"
-        menu(input)
-      elsif input == "alphabetical" || input == "numerical"
-        puts "In #{input} order:"
-        # Pokemon.list(input)
-        menu(input)
-      elsif input == "select"
-        puts "Which Pokémon you would like to see?"
-        select_input = gets.strip.downcase
-        while Pokemon.all.include?(pokemon_select) == true || (pokemon_select.to_i > 0 && pokemon_select.to_i <= 151) do
-      if temp_input == "alphabetical"
-        # Pokemon.find_by_name(pokemon_select)
-        puts "find by name"
-      elsif temp_input == "numerical"
-        # Pokemon.find_by_num(pokemon_select)
-      elsif select_input == "main"
-      elsif
-        puts "Invalid choice, please select from the menu below"
-        menu(select_input)
-      elsif list_all_input != "back"
-        puts "Invalid choice, please select from the menu below"
-        menu(list_all_input)
-      elsif input == "find"
-          Pokemon.find
-      elsif input == "default"
-        input = "default"
-        menu(input)
-      else
-        puts "Invalid choice, please select from the menu below."
-        menu(input)
-      end
-      input = gets.strip.downcase
-    end
+
+    # while input != "exit" do
+    #   if input == "list all"
+    #     puts "How would you like to list?"
+    #     menu(input)
+    #   elsif input == "alphabetical" || input == "numerical"
+    #     find_select = input
+    #     puts "In #{input} order:"
+    #     # Pokemon.list(input)
+    #     menu(input)
+    #   elsif input == "select"
+    #     puts "Which Pokémon you would like to see?"
+    #     select_input = gets.strip.downcase
+    #     while Pokemon.all.include?(select_input) != true || (pokemon_select.to_i <= 0 && pokemon_select.to_i > 151) do
+    #       puts "Could not find Pokémon. Please try again or go back."
+    #       select_input = gets.strip.downcase
+    #       if select_input == "back"
+            
+    #     end
+    #     if select_input == "alphabetical"
+    #       # Pokemon.find_by_name(pokemon_select)
+    #       puts "find by name"
+    #     else
+    #       # Pokemon.find_by_num(pokemon_select)
+    #     end
+    #   elsif select_input == "main"
+    #   elsif
+    #     puts "Invalid choice, please select from the menu below"
+    #     menu(select_input)
+    #   elsif list_all_input != "back"
+    #     puts "Invalid choice, please select from the menu below"
+    #     menu(list_all_input)
+    #   elsif input == "find"
+    #       Pokemon.find
+    #   elsif input == "default"
+    #     input = "default"
+    #     menu(input)
+    #   else
+    #     puts "Invalid choice, please select from the menu below."
+    #     menu(input)
+    #   end
+    #   input = gets.strip.downcase
+    # end
     puts "Shutting down..."
   end
 
@@ -65,6 +72,40 @@ class CommandLineInterface
 
   def display_pokemon_info
     
+  end
+  
+  def start
+    menu_message
+    input = gets.strip.to_i
+    while input != 3 do
+      if input == 1
+        list(input)
+      elsif input == 2
+        find(input)
+      else
+        puts "Invalid choice, please select again: "
+        input = gets.strip.to_i
+      end
+    end
+  end
+  
+  def list(input)
+    puts "Please choose alphabetical or numerical order."
+    menu_message(input)
+    input = gets.strip.to_i
+    while input != 3 do
+      if input == 1
+        # Pokemon.list_alpha
+        select_poke
+      elsif input == 2
+        # Pokemon.list_num
+        select_poke
+      else
+        puts "Invalid choice, please select again."
+        input = gets.strip.to_i
+      end
+    end
+    start
   end
   
   def menu(input)
@@ -85,9 +126,11 @@ class CommandLineInterface
       puts "Menu: Previous | Stats | Weakness | Evolution | Next | Back"
       puts "------------------------------------------------------------"
     else
-      puts "-----------------------------"
-      puts "Menu: List All | Find | Exit"
-      puts "-----------------------------"
+      puts "--------------------------------"
+      puts "Menu: 1. List all Pokémon"
+      puts "      2. Find a specific Pokémon"
+      puts "      3. Exit"
+      puts "--------------------------------"
     end
   end
 end
