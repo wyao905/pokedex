80b4b88d01eb7a3602da24965ad356de295df858
Warren Yao <warrenyca905@gmail.com>
2019-07-13 15:35:59 +0000

    save

--- lib/type.rb
+++ lib/type.rb
@@ -1,3 +1,35 @@
 class Type
+  attr_accessor :name
+  
+  @pokemon = []
+  @weakness = []
+  @strong_against = []
   @@all = []
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def weakness(array)
+    @weakness
+  end
+  
+  def strong_against(array)
+    @strong_against
+  end
+  
+  def show_weakness
+  end
+  
+  def show_strong_against
+  end
+  
+  def self.create(name)
+    new_type = self.new(name)
+    all << new_type
+  end
+  
+  def self.all
+    @@all
+  end
 end
